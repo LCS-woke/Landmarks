db7ee6414701bd55b5ef6e098b88e1a5178ed331
LCS-woke <149812783+LCS-woke@users.noreply.github.com>
2024-01-29 11:15:13 -0500

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by William Oke on 2024-01-29.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
